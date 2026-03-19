8f249bc1882e685d7676965844e7a7f50b0b90c0
Gabriel Costa <costasousagabriel075@gmail.com>
2026-03-19 11:23:44 -0300

    Sexto
    Quase acabando a primeira página, falta os botoes de instalar e criar os outros botoes do header

--- frontend/Site/CrashWare/src/Componentes/ConteudoInicial/ConteudoInicial.jsx
+++ frontend/Site/CrashWare/src/Componentes/ConteudoInicial/ConteudoInicial.jsx
@@ -6,18 +6,54 @@ const ConteudoInicial = () =>
 {
     return (
         <>
-            <div className={Style.Conteudo}>
+            <div className={Style.Conteudo}> {/*Parte de cima*/}
                 <h3>Aprenda Hardware e Software de forma prática e envolvente</h3>
-                
                 <div className={Style.Conteudo_img}>
-                    <img src={FMenina} alt="" />
+                    <img src={FMenina} alt="" /> {/*Foto da parte de cima*/}
                 </div>
-                {/* <hr /> */}
             </div>
-            <Cards />
+            <div> {/*Cards /// coloquei a div só por separação mesmo*/}
+                <Cards />
+            </div>
 
+            <div className={Style.Textos}>
+                <h2>Hardware</h2>
+                <hr />
+
+                <h4>Componentes do Computador</h4>
+                <p>Entenda como funcionam CPU, RAM, SSD, placa-mãe e placa de vídeo e como cada peça influencia no desempenho.</p>
+                <hr />
+
+                <h4>Montagem e Upgrade</h4>
+                <p>Aprenda a montar um computador do zero, substituir peças e melhorar a performance com upgrades estratégicos.</p>
+                <hr />
+
+                <h4>Manutenção e Diagnóstico</h4>
+                <p>Identifique problemas, faça limpeza correta e resolva falhas comuns de hardware.</p>
+                <hr />
+
+                <br />
+
+                <h2>Software</h2>
+                <hr />
+
+                <h4>Sistemas Operacionais</h4>
+                <p>Aprenda a instalar, configurar e otimizar sistemas como Windows e Linux.</p>
+                <hr />
+
+                <h4>Instalação e Configuração de Programas</h4>
+                <p>Saiba instalar softwares, configurar drivers e manter o sistema atualizado e seguro.</p>
+                <hr />
+
+                <h4>Lógica de Programação</h4>
+                <p>Entenda como os programas funcionam e dê os primeiros passos na criação de software. </p>
+                <div className={Style.TextoFinal}>
+                    <h2>Leve o aprendizado com você</h2>
+                    <p>Estude Hardware e Software onde estiver.Aprenda pelo celular, continue no computador e acompanhe seu progresso em tempo real.</p>
+                </div>
+            </div>
         </>
-    )
+    )   
 }
 
 export { ConteudoInicial }
