685116b77b39c66445c1040a0caba5b7f05d8a6e
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-04-14 20:38:17 -0300

    configura CORS no FastAPI

--- backend/main.py
+++ backend/main.py
@@ -18,13 +18,21 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 crashware = FastAPI()
 
 #CORS:
+
+origins = [
+    "https://crashware.onrender.com",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
+
 crashware.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 #Colocando as rotas dentro da API:
 crashware.include_router(auth)
