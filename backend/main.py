92682d5f0c8993e2fb91af3a566478017cd7468f
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-04-26 16:21:23 -0300

    Corrigindo CORS

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,8 @@ crashware = FastAPI()
 
 origins = [
     "https://crashware.onrender.com",
-    "http://localhost:5173",
+    "http://localhost:5173"
+    "http://localhost:5174",
     "http://127.0.0.1:5173"
 ]
 
