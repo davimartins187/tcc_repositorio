485c607913829cc9d210306f613783d054396292
Gabriel Costa <costasousagabriel075@gmail.com>
2026-05-08 17:10:47 -0300

    RESPONSIVIDADEEEEEEEEEEEEEEEEEEEEE E listar conquistas feito

--- frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AbaListarConquistas/AbaListarConquistas.jsx
+++ frontend/Site/CrashWare/src/Paginas/ADM/AbasAdm/AbaListarConquistas/AbaListarConquistas.jsx
@@ -4,7 +4,17 @@ import Style from "./AbaListarConquistas.module.css"
 
 const AbaListarConquistas = () => {
 
-    const [buscar, setBuscar] = useState("")
+    const CONQUISTAS_MOCK = [
+        { id: 1, titulo: 'Conquista de Software', descricao: 'Descrição super divertida sobre a aula concluída para adquirir a conquista.', tipo: 'software' },
+        { id: 2, titulo: 'Conquista de Hardware', descricao: 'Descrição super divertida sobre a aula concluída para adquirir a conquista.', tipo: 'hardware' },
+        { id: 3, titulo: 'Conquista de Hardware', descricao: 'Descrição super divertida sobre a aula concluída para adquirir a conquista.', tipo: 'hardware' },
+        { id: 4, titulo: 'Conquista de Software', descricao: 'Descrição super divertida sobre a aula concluída para adquirir a conquista.', tipo: 'software' },
+        { id: 5, titulo: 'Conquista de Hardware', descricao: 'Descrição super divertida sobre a aula concluída para adquirir a conquista.', tipo: 'hardware' },
+        { id: 6, titulo: 'Conquista de Hardware', descricao: 'Descrição super divertida sobre a aula concluída para adquirir a conquista.', tipo: 'hardware' },
+    ]
+
+    const [conquistas, setConquistas] = useState(CONQUISTAS_MOCK);
+    const [buscar, setBuscar] = useState("");
     return (
         <div className={Style.separarConteudos}>
             <div className={Style.Conteudos}>
@@ -16,9 +26,25 @@ const AbaListarConquistas = () => {
                         onChange={(e) => setBuscar(e.target.value)}
                     />
                     <BotoesForm
-                    className={Style.botaoBuscar}
+                        className={Style.botaoBuscar}
                         texto="Buscar"
                     />
+                </div>
+
+                <div className={Style.Lista}>
+                    <div>
+                        {conquistas.map((c) => (
+                            <div className={Style.ListaConquistas}
+                                key={c.id}
+                            >
+
+                                <div>
+                                    <h6>{c.titulo}</h6>
+                                    <p>{c.descricao}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
