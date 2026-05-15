15b1bb3f2caa54fdfa51509475517b5d41c5149e
FelipeBarbosa044 <felipebarbosaribeiro197@gmail.com>
2026-05-14 22:08:46 -0300

    Arrumando o site...

--- frontend/Site/CrashWare/src/Componentes/Conteudos/conteudoAulas/comoFuncionaEssecurso/ComoFunciona.jsx
+++ frontend/Site/CrashWare/src/Componentes/Conteudos/conteudoAulas/comoFuncionaEssecurso/ComoFunciona.jsx
@@ -1,8 +1,8 @@
 //DAVI É SERIO QUANDO FOR COMMITAR TESTA SE TA FUNCIONANDO TO A 40 MINUTOS ARRUMANDO ISSO POOO
 
-// import Style from "./comoFunciona.module.css";
-// import { ModeloBase } from "../modelo base";
+import Style from "./comoFunciona.module.css";
+import { ModeloBase } from "../modelo base";
 
-// const ComoFunciona = () => {
-//     <modeloBase />
-// }
+const ComoFunciona = () => {
+    <modeloBase />
+}
